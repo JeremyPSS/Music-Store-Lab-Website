856624c59aa094a85a3dd78a9bb0150ee7b90fa7
Jeremy Prieto <jeremy2002_prieto@hotmail.com>
2023-12-23 20:42:12 -0500

    navbar fixed

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,15 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      dropShadow: {
+        '3xl': '0 35px 35px rgba(255, 255, 255, 1)',
+        '4xl': [
+            '0 35px 35px rgba(255, 255, 255, 1)',
+            '0 45px 65px rgba(255, 255, 255, 1)'
+        ]
+      }
+    },
   },
   plugins: [],
 }
